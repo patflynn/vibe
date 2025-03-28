09bf04021db596c8b4d8886aedb3e809933d9461
Patrick Flynn <big.pat@gmail.com>
2025-03-28 13:31:35 -0400

    Update kotlin and fix test import (again?)

--- app/src/androidTest/java/dev/broken/app/vibe/MainActivityTest.kt
+++ app/src/androidTest/java/dev/broken/app/vibe/MainActivityTest.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
